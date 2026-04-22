a14419611d5df120dc421e945f8606f9a9ca0f14
haimamire <ivanmatusevich16@gmail.com>
2026-04-22 11:30:00 -0300

    feat: complete fibonacci with recursion

--- src/fib.js
+++ src/fib.js
@@ -11,3 +11,14 @@ export function fib(length) {
   }
   return array;
 }
+
+export function fibRec(length, array = [0]) {
+  if (array.length === length) return array;
+
+  const lastIndex = array.length - 1;
+  const newValue =
+    array[lastIndex] === 0 ? 1 : array[lastIndex] + array[lastIndex - 1];
+  array.push(newValue);
+
+  return fibRec(length, array);
+}
--- tests/fib.test.js
+++ tests/fib.test.js
@@ -1,4 +1,4 @@
-import { fib } from "../src/fib";
+import { fib, fibRec } from "../src/fib";
 
 describe("fibonacci iteration", () => {
   test("should return 0", () => {
@@ -12,5 +12,20 @@ describe("fibonacci iteration", () => {
   });
   test("should get 8 first digits ", () => {
     expect(fib(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+});
+
+describe("fibonacci recursive", () => {
+  test("should return 0", () => {
+    expect(fibRec(1)).toEqual([0]);
+  });
+  test("should get first 2 digits", () => {
+    expect(fibRec(2)).toEqual([0, 1]);
+  });
+  test("should get 3 first digits ", () => {
+    expect(fibRec(3)).toEqual([0, 1, 1]);
+  });
+  test("should get 8 first digits ", () => {
+    expect(fibRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
   });
 });
