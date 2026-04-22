1fb5590793ebafe9a9be585b44d5336c930a2828
haimamire <ivanmatusevich16@gmail.com>
2026-04-22 14:48:31 -0300

    feat: complete merge sort using recursion

---
+++ src/merge.js
@@ -0,0 +1,24 @@
+export function mergeSort(array) {
+  const arrayLength = array.length;
+
+  if (arrayLength <= 1) return array;
+
+  const leftSide = mergeSort(array.slice(0, arrayLength / 2));
+  const rightSide = mergeSort(array.slice(arrayLength / 2));
+  const sortedArray = [];
+
+  while (true) {
+    if (leftSide.length === 0) {
+      sortedArray.push(...rightSide);
+      break;
+    } else if (rightSide.length === 0) {
+      sortedArray.push(...leftSide);
+      break;
+    }
+
+    if (leftSide[0] < rightSide[0]) sortedArray.push(leftSide.shift());
+    else sortedArray.push(rightSide.shift());
+  }
+
+  return sortedArray;
+}
---
+++ tests/merge.test.js
@@ -0,0 +1,21 @@
+import { mergeSort } from "../src/merge";
+
+describe("merge sort", () => {
+  test("should return empty array", () => {
+    expect(mergeSort([])).toEqual([]);
+  });
+  test("should return same array", () => {
+    expect(mergeSort([73])).toEqual([73]);
+  });
+  test("should return merged array", () => {
+    expect(mergeSort([0, 3, 1, 4, 2])).toEqual([0, 1, 2, 3, 4]);
+  });
+  test("should sort array with duplicate value", () => {
+    expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([
+      0, 1, 1, 2, 3, 5, 8, 13,
+    ]);
+  });
+  test("should work with large numbers", () => {
+    expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
+  });
+});
